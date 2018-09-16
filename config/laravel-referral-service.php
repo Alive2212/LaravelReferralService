71277e3b834b8e6c8dc790ce898f5b0536871e80
Pooyadch <pooyad.d2009@gmail.com>
2018-09-16 18:34:12 +0430

    add sms message to config

--- config/laravel-referral-service.php
+++ config/laravel-referral-service.php
@@ -3,5 +3,10 @@
 return [
     'default_message' => 'همه چی درسته',
     'download_view' => 'vendor.alive2212.download',
-    'pre_register_api' => '/api/v1/custom/alive/referral/preregister'
+    'pre_register_api' => '/api/v1/custom/alive/referral/preregister',
+    'download_message' => 'اشتراک شما با موفقیت ثبت شد 
+با استفاده از لینک زیر اپلیکیشن را دانود نمایید
+https://besparapp.com/download
+',
+
 ];
--- src/Http/Controllers/AliveReferralPreRegisterController.php
+++ src/Http/Controllers/AliveReferralPreRegisterController.php
@@ -4,15 +4,20 @@ namespace Alive2212\LaravelReferralService\Http\Controllers;
 
 use Alive2212\LaravelReferralService\AliveReferralPreRegister;
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\View;
 
 class AliveReferralPreRegisterController extends Controller
 {
-
-    public static function createdd(Request $request)
+    private $kaveUrl = 'https://api.kavenegar.com/v1/6A596E65546566704B7677554C334342516472574A413D3D/sms/send.json' ;
+    public function create(Request $request)
     {
+        $smsJson = [
+            'receptor' => $request->input('phone_number'),
+            'message' => (config('laravel-referral-service.download_message'))
+        ];
         $preRegisterData = [
             'user_id' => $request->input('user_id'),
             'phone_number' => $request->input('phone_number'),
@@ -20,6 +25,10 @@ class AliveReferralPreRegisterController extends Controller
         ];
         $preRegister = new AliveReferralPreRegister();
         $preRegister = $preRegister->firstOrCreate($preRegisterData);
+        $client = new Client();
+        $client = $client->Post($this->kaveUrl, [
+            'form_params' => $smsJson
+        ]);
         return View::make(config('laravel-referral-service.download_view'));
     }
 }
