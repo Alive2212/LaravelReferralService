db10ab4facfd8c98fb415aa98fc4aec20406020a
Pooyadch <pooyad.d2009@gmail.com>
2018-09-17 11:11:57 +0430

    migration edit

--- database/migrations/2018_08_15_100628_create_alive_referral_pre_registers_table.php
+++ database/migrations/2018_08_15_100628_create_alive_referral_pre_registers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePreRegistersTable extends Migration
+class CreateAliveReferralPreRegistersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class CreatePreRegistersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pre_registers');
+        Schema::dropIfExists('alive_referral_pre_registers');
     }
 }
--- database/migrations/2018_08_15_101629_create_alive_referral_processes_table.php
+++ database/migrations/2018_08_15_101629_create_alive_referral_processes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProcessesTable extends Migration
+class CreateAliveReferralProcessesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -35,6 +35,6 @@ class CreateProcessesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('processes');
+        Schema::dropIfExists('alive_referral_processes');
     }
 }
