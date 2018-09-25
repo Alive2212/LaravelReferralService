c8cf55992f4d64ca86cc396fee8c14098ae73c73
Pooyadch <pooyad.d2009@gmail.com>
2018-09-25 11:59:13 +0330

    user exist check

--- src/Http/Controllers/AliveReferralPreRegisterController.php
+++ src/Http/Controllers/AliveReferralPreRegisterController.php
@@ -4,6 +4,7 @@ namespace Alive2212\LaravelReferralService\Http\Controllers;
 
 use Alive2212\LaravelReferralService\AliveReferralPreRegister;
 use App\Http\Controllers\Controller;
+use App\User;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
@@ -14,14 +15,41 @@ class AliveReferralPreRegisterController extends Controller
     private $kaveUrl = 'https://api.kavenegar.com/v1/6A596E65546566704B7677554C334342516472574A413D3D/sms/send.json' ;
     public function create(Request $request)
     {
-        ;
+        if (is_null($request->input('phone_number'))) {
+            return View::make(config('laravel-referral-service.download_view'));
+        }
+        $userPhoneNumber = $request->input('phone_number');
+        if($request->input('user_number') == $userPhoneNumber){
+            return View::make(config('laravel-referral-service.download_view'));
+        }
+        $userExist = $this->userExistCheck($userPhoneNumber);
+        if (!is_null($userExist)) {
+            return View::make(config('laravel-referral-service.download_view'));
+        }
+        $this->storePreRegister($request);
+        return View::make(config('laravel-referral-service.download_view'));
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function sendSms(Request $request)
+    {
         $smsJson = [
             'receptor' => $request->input('phone_number'),
             'message' => (config('laravel-referral-service.download_message'))
         ];
-        if($request->input('user_number') == $request->input('phone_number')){
-            return View::make(config('laravel-referral-service.download_view'));
-        }
+        $client = new Client();
+        $client = $client->Post($this->kaveUrl, [
+            'form_params' => $smsJson
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function storePreRegister(Request $request)
+    {
         $preRegisterData = [
             'user_id' => $request->input('user_id'),
             'phone_number' => $request->input('phone_number'),
@@ -29,10 +57,17 @@ class AliveReferralPreRegisterController extends Controller
         ];
         $preRegister = new AliveReferralPreRegister();
         $preRegister = $preRegister->firstOrCreate($preRegisterData);
-        $client = new Client();
-        $client = $client->Post($this->kaveUrl, [
-            'form_params' => $smsJson
-        ]);
-        return View::make(config('laravel-referral-service.download_view'));
+        $this->sendSms($request);
+    }
+
+    /**
+     * @param $userPhoneNumber
+     * @return mixed
+     */
+    public function userExistCheck($userPhoneNumber)
+    {
+        $user = new User();
+        $userExist = $user->where('phone_number', '=', $userPhoneNumber)->first();
+        return $userExist;
     }
 }
